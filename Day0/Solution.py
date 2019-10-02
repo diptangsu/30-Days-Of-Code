cca2a4401faebf8426313772339149b3cd871d82
diptangsu <deep.dippu@gmail.com>
2019-10-02 17:26:20 +0530

    pep8 formatted code and added support for py3

--- Day0/Solution.py
+++ Day0/Solution.py
@@ -1,6 +1,16 @@
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-inputString = raw_input()
+from __future__ import print_function
+import sys
+
+PY_VERSION = sys.version_info.major
+
+
+# Read a full line of input from stdin and save it to our dynamically
+# typed variable, input_string.
+if PY_VERSION == 2:
+    inputString = raw_input()
+else:
+    inputString = input()
 
 # Print a string literal saying "Hello, World." to stdout.
-print 'Hello, World.'
-print inputString
+print('Hello, World.')
+print(inputString)
--- Day1/Solution.py
+++ Day1/Solution.py
@@ -1,11 +1,20 @@
+from __future__ import print_function
+import sys
+
+PY_VERSION = sys.version_info.major
+
 # Read and save an integer, double, and String to your variables.
-j = int (raw_input())
+j = int(raw_input())
 e = float(raw_input())
-t = raw_input()
+
+if PY_VERSION == 2:
+    t = raw_input()
+else:
+    t = input()
 # Print the sum of both integer variables on a new line.
-print i+j
+print(i + j)
 # Print the sum of the double variables on a new line.
-print d+e
+print(d + e)
 # Concatenate and print the String variables on a new line
 # The 's' variable above should be printed first.
-print s+t
+print(s + t)
